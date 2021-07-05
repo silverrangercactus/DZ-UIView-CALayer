64e64b454828dcb86454b88663199f3f6ca35306
silverrangercactus <silverrangercactus@gmail.com>
2021-07-05 17:24:48 +0500

    50% new dz

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -10,89 +10,76 @@ import UIKit
 
 class ProfileHeaderView: UIView {
 
-    let button = UIButton()
     let avatarPicture = UIImage(named: "ava")
-    let avatarView = UIImageView()
-    let topicView = UITextView()
-    let commentView = UITextView()
-    let setStatusView = UITextField()
-    var tittle = String()
+    var title: String = ""
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
+    @IBOutlet weak var avatarImage: UIImageView! {
+        didSet {
+            avatarImage.image = avatarPicture
+            avatarImage.layer.masksToBounds = true
+            avatarImage.layer.cornerRadius = 60
+            avatarImage.layer.borderWidth = 3
+            avatarImage.layer.borderColor = UIColor.white.cgColor
+        }
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        button.frame = CGRect(x: 16, y: safeAreaInsets.top + 152, width: self.frame.width - 32, height: 50)
-        avatarView.frame = CGRect(x: 16, y: self.safeAreaInsets.top + 16, width: 120, height: 120)
-        topicView.frame = CGRect(x: self.frame.width/3, y: self.safeAreaInsets.top + 27, width: 250, height: 30)
-        commentView.frame = CGRect(x: self.frame.width/3, y: safeAreaInsets.top + 84, width: 250, height: 30)
-        setStatusView.frame = CGRect(x: self.frame.width/3 + 10, y: safeAreaInsets.top + 110, width: 250, height: 40)
-        
+    @IBOutlet weak var topicView: UITextView! {
+        didSet {
+            topicView.text = "Miss cowboy Cactus"
+            topicView.backgroundColor = .none
+            topicView.textColor = .black
+            topicView.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+            topicView.textAlignment = .left
+        }
     }
     
-    private func setupViews() {
-        addSubview(button)
-        addSubview(avatarView)
-        addSubview(topicView)
-        addSubview(commentView)
-        addSubview(setStatusView)
-        
-        button.layer.cornerRadius = 10
-        button.backgroundColor = .systemBlue
-        button.setTitleColor(.white, for: .normal)
-        button.setTitle("Show status", for: .normal)
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOffset = CGSize(width: 10, height: 10)
-        button.layer.shadowOpacity = 0.7
-        button.layer.shadowRadius = 10
-        button.addTarget(self, action: #selector(actionButtonPressed), for: .touchUpInside)
-        
-        avatarView.image = avatarPicture
-        avatarView.layer.masksToBounds = true
-        avatarView.layer.cornerRadius = 60
-        avatarView.layer.borderWidth = 3
-        avatarView.layer.borderColor = UIColor.white.cgColor
-        
-        topicView.text = "Miss cowboy Cactus"
-        topicView.backgroundColor = .none
-        topicView.textColor = .black
-        topicView.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        topicView.textAlignment = .left
-        
-        commentView.text = "I will wait your text"
-        commentView.backgroundColor = .none
-        commentView.textColor = .gray
-        commentView.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        commentView.textAlignment = .left
-        
-       
-        setStatusView.placeholder = "Set status"
-        setStatusView.backgroundColor = .white
-        setStatusView.tintColor = .black
-        setStatusView.font = UIFont.systemFont(ofSize: 15, weight: .regular)
-        setStatusView.layer.cornerRadius = 12
-        setStatusView.layer.borderWidth = 1
-        setStatusView.layer.borderColor = UIColor.black.cgColor
-        setStatusView.addTarget(self, action: #selector(actionButtonSetStatus), for: .editingChanged)
+    @IBOutlet weak var button: UIButton! {
+        didSet {
+            button.layer.cornerRadius = 10
+            button.backgroundColor = .systemBlue
+            button.setTitleColor(.white, for: .normal)
+            button.setTitle("Show status", for: .normal)
+            button.layer.shadowColor = UIColor.black.cgColor
+            button.layer.shadowOffset = CGSize(width: 10, height: 10)
+            button.layer.shadowOpacity = 0.7
+            button.layer.shadowRadius = 10
+        }
     }
     
-    
-    @objc func actionButtonPressed() {
-        commentView.text = tittle
-        
+    @IBOutlet weak var commentView: UITextView! {
+        didSet {
+            commentView.text = "I will wait your text"
+            commentView.backgroundColor = .none
+            commentView.textColor = .gray
+            commentView.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+            commentView.textAlignment = .left
+        }
     }
+  
     
-    @objc func actionButtonSetStatus(_ textField: UITextField) {
-        tittle = textField.text ?? ""
+    @IBOutlet weak var setStatusView: UITextField! {
+        didSet {
+            setStatusView.placeholder = "Set status"
+            setStatusView.backgroundColor = .none
+            setStatusView.layer.masksToBounds = true
+            setStatusView.layer.backgroundColor = .none
+            setStatusView.tintColor = .black
+            setStatusView.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+            setStatusView.layer.cornerRadius = 12
+            setStatusView.layer.borderWidth = 1
+            setStatusView.layer.borderColor = UIColor.black.cgColor
+        }
     }
+ 
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    @IBAction func statusAction(_ sender: Any) {
+        title = setStatusView.text ?? ""
     }
     
     
+    @IBAction func actionButton(_ sender: Any) {
+        commentView.text = title
+    }
     
 }
+    
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     let profileView = ProfileHeaderView()
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
@@ -22,16 +22,7 @@ class ProfileViewController: UIViewController {
    
     override func viewWillLayoutSubviews() {
         view.addSubview(profileView)
-        profileView.frame = self.view.frame
+        
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+ 
 }
